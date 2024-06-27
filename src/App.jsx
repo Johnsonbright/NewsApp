af1d9451b1bb3f0f6846736a8fd0bb50957842d9
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-06-27 12:21:19 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Newsboard from "./components/Newsboard";
+import { useState } from "react";
+
+
+function App() {
+  const [category, setCategory] = useState("general")
+  
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <Newsboard category={category}  />
+    </div>
+  );
+}
+
+export default App;
