0accf626e43179bf02df42b1393225080a68ba32
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-06-28 12:44:14 -0700

    changes

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -7,7 +7,7 @@ export default function Newsboard({category}) {
   useEffect(()=> {
 let url =`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
 fetch(url).then(response=> response.json()).then(data => setArticles(data.articles));
-  }, [])
+  }, [category])
 
   return (
     <div>
